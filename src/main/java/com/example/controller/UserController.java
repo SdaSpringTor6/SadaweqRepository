0f43771eccba7e3bac295654984a46c0a22d48cb
Norbert <syskaxiii@gmail.com>
2020-05-24 12:18:54 +0200

    init commit

---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.example.controller;
+
+import com.example.dao.UserDAO;
+import com.example.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    UserDAO userDao;
+
+    @RequestMapping("/main")
+    public String mainPage(Model model) {
+        model.addAttribute("users", userDao.getUsers());
+        return "index";
+    }
+    @RequestMapping("/main/{id}")
+    public String mainPageId(@PathVariable("id") int id, Model model){
+        model.addAttribute("user", userDao.getUserById(id));
+        return "index";
+    }
+    @RequestMapping("/post")
+    public String postPage(Model model) {
+        model.addAttribute("user", new User());
+        return "post";
+    }
+    //    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping("/save")
+    public String postAction(User user){
+        user.setId(5);
+        return "index";
+    }
+
+}
---
+++ src/main/java/com/example/dao/UserDAO.java
@@ -0,0 +1,31 @@
+package com.example.dao;
+
+import com.example.model.User;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserDAO {
+
+    private List<User> users = new ArrayList<>();
+
+    @PostConstruct  //zadzieje sie podczas inicjalizacji beana
+    public void init() {
+        users.add(new User(0, "Romek", "Boot"));
+        users.add(new User(1, "Cypis", "Kaszalot"));
+        users.add(new User(2, "Maria", "Magdalena"));
+        users.add(new User(3, "Jan", "Paweł"));
+        users.add(new User(4, "Adolf", "SamWieszJaki"));
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public User getUserById(int id) {
+        return users.get(id);
+    }
+}
